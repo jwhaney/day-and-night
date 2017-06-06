e3202743892369698230011b48007d1788632137
geojwh <geojwh@gmail.com>
2017-06-06 12:35:45 -0500

    cool leaflet map - earth day and night

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # day-and-night
-a leaflet map showing areas of day and night on earth
+a leaflet map using the Leaflet plugin called Terminator (yes, coolest name ever). the plugin allows overlay of day and night regions on earth. this particular example map shows current areas of day and night with auto-updates every 0.5 seconds for a near real-time geospatial view. you can zoom in and actually watch the night time area move across the earth. view at [http://johnwhaney.com/day-and-night](http://johnwhaney.com/day-and-night).
+
+credit to [Jorg Dietrich](https://github.com/joergdietrich) for developing a really cool Leaflet plugin (with a really cool name).
+view his documentation for [Leaflet Terminator](https://github.com/joergdietrich/Leaflet.Terminator) @ [joergdietrich.github.io/Leaflet.Terminator/](http://joergdietrich.github.io/Leaflet.Terminator/)
---
+++ js/map.js
@@ -0,0 +1,34 @@
+//create var bounds
+var northWest = L.latLng(70, -141),
+	southEast = L.latLng(-53, 170),
+	bounds = L.latLngBounds(northWest, southEast);
+
+//create map and use stamen terrain base
+var layer = new L.StamenTileLayer("terrain");
+var map = new L.Map("map", {
+    zoom: 3,
+	minZoom: 3
+});
+		
+map.addLayer(layer);
+
+map.fitBounds(bounds);
+
+/*
+load terminator and use script to auto udpate every 0.5 seconds;
+credit to Jorg Dietrich's example at joergdietrich.github.io/Leaflet.Terminator/
+*/
+var t = L.terminator();
+		
+t.addTo(map);
+setInterval(function(){updateTerminator(t)}, 500);
+function updateTerminator(t) {
+  	var t2 = L.terminator();
+  	t.setLatLngs(t2.getLatLngs());
+  	t.redraw();
+}
+
+//custom easy button control, zoom to var bounds
+L.easyButton('fa-globe fa-lg', function(){
+    map.fitBounds(bounds)
+}).addTo(map);
